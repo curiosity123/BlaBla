e099bbddf9c81a7c7f96201b5080f51a4616b804
Łukasz <ukasz@UKASZ-KOMPUTER>
2017-11-12 23:41:20 +0100

    bla bla server simplest imprementation

---
+++ BlaBla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlaBla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
